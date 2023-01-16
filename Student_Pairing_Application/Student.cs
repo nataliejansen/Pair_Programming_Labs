a959832ecc07ca6c619fd34cfbdfb4d62bd46356
nataliekuechler <nataliekuechler@outlook.com>
2023-01-16 14:27:43 -0600

    student

---
+++ Student_Pairing_Application/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Pairing_Application
+{
+    internal class Student
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a student's name");
+
+            string Name = Console.ReadLine().ToLower().Trim();
+
+            List<string> students = new List<string>();
+            students.Add(Name);
+
+
+
+        }
+    }
+
+    
+}
